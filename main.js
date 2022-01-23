4ec73896b4999fc446a1840af3c59422f5d97b67
musa1992 <mosesbwire91@gmail.com>
2022-01-23 15:08:06 +0300

    finish overlay styling

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 let menuIcon = document.getElementById('menu-icon');
 let menu = document.getElementById('navigation');
 let closeBtn = document.getElementById('close-btn');
+let overlay = document.querySelector('.overlay')
 
 menuIcon.addEventListener('click',()=>{
     menu.style.left = "0"
+    overlay.style.display = "block"
+
 })
 
 closeBtn.addEventListener('click', ()=>{
     menu.style.left = "-80%"
+    overlay.style.display = "none"
 })
