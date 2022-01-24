1461ecd9d19fd63b8c5d63643db936530e4ace55
musa1992 <mosesbwire91@gmail.com>
2022-01-25 00:28:26 +0300

    add partially finished product carousel, scrolls to both and left but continues scrolling after last image.

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ let menuIcon = document.getElementById('menu-icon');
 let menu = document.getElementById('navigation');
 let closeBtn = document.getElementById('close-btn');
 let overlay = document.querySelector('.overlay')
+let previousBtn = document.getElementById('icon-previous')
+let nextBtn = document.getElementById('icon-next')
+let productCarousel = document.querySelector('.product-image-container')
+let productImages = Array.from(productCarousel.children)
+let width = productCarousel.clientWidth
 
+let leftFactor = productCarousel.getClientRects()[0].left
 menuIcon.addEventListener('click',()=>{
     menu.style.left = "0"
     overlay.style.display = "block"
@@ -12,4 +18,13 @@ menuIcon.addEventListener('click',()=>{
 closeBtn.addEventListener('click', ()=>{
     menu.style.left = "-80%"
     overlay.style.display = "none"
+})
+nextBtn.addEventListener('click',()=>{
+    let leftPos = productCarousel.getClientRects()[0].left
+    productCarousel.style.left = ((-1 * width) + leftPos )-leftFactor  + "px"
+})
+
+previousBtn.addEventListener('click',()=>{
+    let x = productCarousel.getClientRects()[0].left + width
+    productCarousel.style.left = (x-leftFactor) +"px"
 })
