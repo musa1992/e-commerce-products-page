c7af6c566f9b956577a2a79860153ad2e5b97036
musa1992 <mosesbwire91@gmail.com>
2022-01-25 22:19:47 +0300

    fix scroll issue on product carousel for mobile view

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let nextBtn = document.getElementById('icon-next')
 let productCarousel = document.querySelector('.product-image-container')
 let productImages = Array.from(productCarousel.children)
 let width = productCarousel.clientWidth
-
 let leftFactor = productCarousel.getClientRects()[0].left
+
 menuIcon.addEventListener('click',()=>{
     menu.style.left = "0"
     overlay.style.display = "block"
@@ -20,11 +20,25 @@ closeBtn.addEventListener('click', ()=>{
     overlay.style.display = "none"
 })
 nextBtn.addEventListener('click',()=>{
+    previousBtn.style.visibility ="visible"
     let leftPos = productCarousel.getClientRects()[0].left
     productCarousel.style.left = ((-1 * width) + leftPos )-leftFactor  + "px"
+    
+    let end = (((productCarousel.childElementCount - 2)* width)-leftFactor)*-1
+    if (leftPos < end){
+        nextBtn.style.visibility = "hidden"
+    }
+
 })
 
 previousBtn.addEventListener('click',()=>{
-    let x = productCarousel.getClientRects()[0].left + width
+    nextBtn.style.visibility = "visible"
+    let x= productCarousel.getClientRects()[0].left + width
     productCarousel.style.left = (x-leftFactor) +"px"
+    let end = (((productCarousel.childElementCount - 3)* width)-leftFactor)*-1
+   
+    if (x > end){
+        previousBtn.style.visibility = "hidden"
+    }
+
 })
