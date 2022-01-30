1fd49f66141baf96572dc182ea548bbf020564ab
musa1992 <mosesbwire91@gmail.com>
2022-01-30 13:44:56 +0300

    enable items to be added to cart when add to cart button is clicked

--- main.js
+++ main.js
@@ -13,6 +13,17 @@ let count = document.querySelector('.count')
 let width = productCarousel.clientWidth
 let leftFactor = productCarousel.getClientRects()[0].left
 const header = document.querySelector('header')
+const addToCart = document.querySelector('.add-to-cart')
+let countItems = document.querySelector('.items-count-wrapper')
+const productPrice = document.getElementById('price').textContent;
+const productName = document.getElementById('product-name').textContent
+const productImageUrl = document.getElementById('cart-product-thumbnail').getAttribute("src")
+
+const cartTemplate = document.getElementById('cart-template')
+
+const cartItemSection = document.querySelector('.cart-items-section')
+
+
 
 menuIcon.addEventListener('click',()=>{
     menu.style.left = "0"
@@ -31,7 +42,7 @@ cart.addEventListener('click',()=>{
     let top = header.getClientRects()[0].bottom
     
     if(cartBasket.getClientRects()[0].top == -300){
-        cartBasket.style.top = top +"px"
+        cartBasket.style.top = (top + 10 )+"px"
     }else {
         cartBasket.style.top = "-300px"
     }
@@ -73,7 +84,53 @@ counter.addEventListener('click',(e)=>{
             items = 1
         }
         count.textContent = items
+    }   
+})
+
+const userCart =(()=>{
+    let items = []
+    let cartItem = {}
+    const addItem = (item,quantity)=>{
+        cartItem.item = item
+        cartItem.quantity = quantity
+        items.push(cartItem)
     }
 
+    const listItems = ()=>{
+        return items
+    }
+
+    return {addItem, listItems}
+})();
+
+const productFactory = (productName,price,imageUrl)=>{
+    return {productName, price, imageUrl}
+}
+
+addToCart.addEventListener('click',()=>{
+    countItems.textContent = count.textContent
+    countItems.style.visibility ="visible"
+
+    let product = productFactory(productName,productPrice,productImageUrl)
+    userCart.addItem(product,count.textContent)
+
+    const item = cartTemplate.content.cloneNode(true).children[0]
+    const img = item.querySelector('.img-thumbnail')
+    const name = item.querySelector('.cart-product-name')
+    const quantity = item.querySelector('.qty-amnt')
+
+    userCart.listItems().forEach(element => {
+        img.setAttribute("src",element.item.imageUrl)
+        name.textContent = element.item.productName
+        let price = (element.item.price).split("$")
+        price = price[1]
+        totalPrice = parseInt(price) * parseInt(element.quantity)
+        quantity.textContent = `${element.item.price} x ${element.quantity} ${totalPrice}.00`
     
+    })
+    cartItemSection.append(item)
+
+
 })
+
+
